f70f793e72270dbca7e42ae6268f3a2bcf656010
Etufrank <frankrazni@gmail.com>
2026-06-22 22:55:02 +0300

    Database.java

--- src/tools/Database.java
+++ src/tools/Database.java
@@ -2,14 +2,27 @@ package tools;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Database {
-    private static final String URL = "jdbc:postgresql://localhost:5432/mahery_vaika";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "POSTGRES"; 
 
-    public Connection dbconnect() throws Exception {
-        Class.forName("org.postgresql.Driver");
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+    private String url = "jdbc:postgresql://localhost:5432/mahery_vaika";
+    private String user = "postgres";
+    private String password = "root";
+
+    public Connection dbconnect() {
+        Connection c = null;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection(url, user, password);
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("⚠️ Driver PostgreSQL introuvable !");
+        } catch (SQLException e) {
+            System.out.println("⚠️ Erreur SQL : " + e.getMessage());
+        }
+
+        return c;
     }
 }
